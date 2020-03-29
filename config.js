1fb100a64c680844809af208d216987d87413744
ritikdhayal5473 <58858175+ritikdhayal5473@users.noreply.github.com>
2020-03-29 06:51:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  
+  token: 'NjkzNTIzODc4MzE1Njg3OTM2.Xn-UeQ.iV-839oqBpKe4RWwJouHTDi9d7Y', 
+  clockchannel: '693523513616892027', 
+  timezone: 'Asia/Kolkata',
+  format: 'HH:mm (z)',
+  updateinterval: 60000,
+ 
+}
