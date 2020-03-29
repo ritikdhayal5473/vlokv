f86ac010e72ad5d165d01eb81e53ea39cd79c4e3
ritikdhayal5473 <58858175+ritikdhayal5473@users.noreply.github.com>
2020-03-29 07:02:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   
-  token: 'NjkzNTM5NjQ0Mzk0MTc2NTQz.Xn_5RQ.hExeyo-z6J35qWWkxcPxfpagkRA', 
+  token: 'NjkzNTM5NjQ0Mzk0MTc2NTQz.Xn_6jA.I6_VD2fPrNwII5n_fuYvD-Thr_w', 
   clockchannel: '693533177414811730', 
   timezone: 'Asia/Kolkata',
   format: 'HH:mm (z)',
